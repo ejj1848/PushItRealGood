c13730de93c1fdbdc956b1f86729ae4d3e7e8a57
Eric Braun <ericbraun@outlook.com>
2017-07-25 14:35:56 -0500

    added name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Eric Braun
         EricJ ericJ = new EricJ("Eric is the kewlest");
         System.out.println(ericJ.getEricsDescription());
 
