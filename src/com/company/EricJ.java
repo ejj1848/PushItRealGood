e1a30b42351e1ac2c4da04f0a9ef96e0241b733b
Eric.Johnson <eric.johnson@astontech.com>
2017-07-25 11:18:17 -0500

    Initial commit

---
+++ src/com/company/EricJ.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class EricJ {
+
+    String ericsDescription ;
+
+
+    public EricJ(String ericsDescription) {
+        this.ericsDescription = ericsDescription;
+    }
+
+    public String getEricsDescription() {
+        return ericsDescription;
+    }
+
+    public void setEricsDescription(String ericsDescription) {
+        this.ericsDescription = ericsDescription;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EricJ ericJ = new EricJ("Eric is the kewlest");
+        System.out.println(ericJ.getEricsDescription());
+    }
+}
